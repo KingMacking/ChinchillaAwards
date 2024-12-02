a871784bdd66730ec33ff6ab3d2161a84eaaf9a1
KingMacking <matiasatzoridev@gmail.com>
2024-12-02 19:53:29 -0300

    FEAT: Add selection between categories and clips to vote

--- src/components/LoginScreen/LoginScreen.jsx
+++ src/components/LoginScreen/LoginScreen.jsx
@@ -17,10 +17,11 @@ function LoginScreen() {
 		if (diff <= 0) {
 			setTimeLeft("¡El evento ha comenzado!");
 		} else {
+			const days = Math.floor(diff / (1000 * 60 * 60 * 24));
 			const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 			const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
 			const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-			setTimeLeft(`${hours}h ${minutes}m ${seconds}s`);
+			setTimeLeft(`${days} : ${hours} : ${minutes} : ${seconds} :`);
 		}
 	};
 
