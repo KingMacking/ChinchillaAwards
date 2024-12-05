e215869b73ad9814ec8f2501d12f204bb4834a43
KingMacking <matiasatzoridev@gmail.com>
2024-12-05 17:32:19 -0300

    FEAT: Create results view

--- src/components/ResultsSection/ResultsSection.jsx
+++ src/components/ResultsSection/ResultsSection.jsx
@@ -94,32 +94,32 @@ const ResultsSection = ({ selectedTab, categories }) => {
 	return (
 		<div className='w-full max-w-4xl p-4 mx-auto text-white bg-[#000816] bg-opacity-50 rounded-lg shadow-md backdrop-blur-md gap-4 flex items-start'>
 			{/* Lista de categorías */}
-				<div className='flex flex-col'>
-					{categories.map((item) => {
-						return (
-							<button
-								key={item.id}
-								className={`px-4 py-2 text-lg transition-all duration-200 rounded-lg cursor-pointer category-item border border-transparent text-white uppercase hover:border-secondary ${
-									selectedCategory === item.id
-										? "bg-primary text-white font-bold hover:text-white"
-										: ""
-								} `}
-								onClick={() => setSelectedCategory(item.id)}
-								type='button'
-							>
-								{item.name}
-							</button>
-						);
-					})}
-				</div>
+			<div className='flex flex-col'>
+				{categories.map((item) => {
+					return (
+						<button
+							key={item.id}
+							className={`px-4 py-2 text-lg transition-all duration-200 rounded-lg cursor-pointer category-item border border-transparent text-white uppercase hover:border-secondary ${
+								selectedCategory === item.id
+									? "bg-primary text-white font-bold hover:text-white"
+									: ""
+							} `}
+							onClick={() => setSelectedCategory(item.id)}
+							type='button'
+						>
+							{item.name}
+						</button>
+					);
+				})}
+			</div>
 
-				{/* Resultados */}
-				<div className=''>
-					{selectedCategory && (
-						<div className='result-details'>
-							{/* Top 1 */}
-							<div className='flex mb-8'>
-								{/* {currentResult?.results.map((result, index) => (
+			{/* Resultados */}
+			<div className=''>
+				{selectedCategory && (
+					<div className='result-details'>
+						{/* Top 1 */}
+						<div className='flex mb-8'>
+							{/* {currentResult?.results.map((result, index) => (
 									<div key={result.participantId} className='mb-4'>
 										{index === 0 && (
 											<div
@@ -144,50 +144,46 @@ const ResultsSection = ({ selectedTab, categories }) => {
 										)}
 									</div>
 								))} */}
-                                <div key={currentResult?.results[0].participantId} className='mb-4'>
-										{index === 0 && (
-											<div
-												className={`bg-[#000816] bg-opacity-60 p-6 flex-1 flex flex-col gap-4 rounded-lg border-2 border-primary`}
-											>
-												<div className='flex items-center gap-6'>
-													<img
-														src={currentResult?.results[0].img}
-														alt={currentResult?.results[0].name}
-														className='object-cover w-64 h-auto rounded-md'
-													/>
-													<div>
-														<h3 className='text-3xl text-primary'>
-															{currentResult?.results[0].name}
-														</h3>
-														<p className='text-white'>
-															Puntos: {currentResult?.results[0].points}
-														</p>
-													</div>
-												</div>
-											</div>
-										)}
+							<div key={currentResult?.results[0].participantId} className='mb-4'>
+								<div
+									className={`bg-[#000816] bg-opacity-60 p-6 flex-1 flex flex-col gap-4 rounded-lg border-2 border-primary`}
+								>
+									<div className='flex items-center gap-6'>
+										<img
+											src={currentResult?.results[0].img}
+											alt={currentResult?.results[0].name}
+											className='object-cover w-64 h-auto rounded-md'
+										/>
+										<div>
+											<h3 className='text-3xl text-primary'>
+												{currentResult?.results[0].name}
+											</h3>
+											<p className='text-white'>
+												Puntos: {currentResult?.results[0].points}
+											</p>
+										</div>
 									</div>
-							</div>
-
-							{/* Resto de los participantes */}
-							<div>
-								{currentResult?.results.map((result, index) => (
-									<div key={result.participantId}>
-										{index > 0 && (
-											<div className='bg-[#000816] bg-opacity-60 px-4 py-2 rounded-lg mb-2'>
-												<h3 className='text-xl text-primary'>
-													{result.name}
-												</h3>
-												<p className='m-0 text-white'>
-													Puntos: {result.points}
-												</p>
-											</div>
-										)}
-									</div>
-								))}
+								</div>
 							</div>
 						</div>
-					)}
+
+						{/* Resto de los participantes */}
+						<div>
+							{currentResult?.results.map((result, index) => (
+								<div key={result.participantId}>
+									{index > 0 && (
+										<div className='bg-[#000816] bg-opacity-60 px-4 py-2 rounded-lg mb-2'>
+											<h3 className='text-xl text-primary'>{result.name}</h3>
+											<p className='m-0 text-white'>
+												Puntos: {result.points}
+											</p>
+										</div>
+									)}
+								</div>
+							))}
+						</div>
+					</div>
+				)}
 			</div>
 		</div>
 	);
