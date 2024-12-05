d4fe340b0011a8b4b83b1ed03b8938a65efa9c08
KingMacking <matiasatzoridev@gmail.com>
2024-12-05 20:40:01 -0300

    FEAT: Add % of votes aside to the points

--- src/components/ResultsSection/ResultsSection.jsx
+++ src/components/ResultsSection/ResultsSection.jsx
@@ -59,8 +59,12 @@ const ResultsSection = ({ selectedTab, categories, goBack }) => {
 					return {
 						participantId,
 						points,
-						name: selectedTab === 'categories' ? participant.name : participant.clipName,
-						img: selectedTab === 'categories' ? `/assets/participants-pictures/${participant.image}` : null,
+						name:
+							selectedTab === "categories" ? participant.name : participant.clipName,
+						img:
+							selectedTab === "categories"
+								? `/assets/participants-pictures/${participant.image}`
+								: null,
 					};
 				});
 
@@ -96,8 +100,8 @@ const ResultsSection = ({ selectedTab, categories, goBack }) => {
 			{/* Lista de categorías */}
 			<div className='flex flex-col gap-1 min-w-64'>
 				{categories.map((item) => {
-                    console.log(currentResult);
-                    
+					console.log(currentResult);
+
 					return (
 						<button
 							key={item.id}
@@ -113,7 +117,11 @@ const ResultsSection = ({ selectedTab, categories, goBack }) => {
 						</button>
 					);
 				})}
-				<button type="button" onClick={goBack} className='px-4 py-2 text-lg font-bold text-white uppercase transition-all duration-200 border border-transparent rounded-lg cursor-pointer category-item bg-primary'>
+				<button
+					type='button'
+					onClick={goBack}
+					className='px-4 py-2 text-lg font-bold text-white uppercase transition-all duration-200 border border-transparent rounded-lg cursor-pointer category-item bg-primary'
+				>
 					Volver atrás
 				</button>
 			</div>
@@ -122,70 +130,110 @@ const ResultsSection = ({ selectedTab, categories, goBack }) => {
 			<div className='flex-[1]'>
 				{selectedCategory && (
 					<div className='result-details'>
-						{/* Top 1 */}
-						<div className='flex mb-4'>
-							<div
-								key={
-									currentResult?.results[0].participantId ||
-									currentResult?.results[0].id
-								}
-								className='w-full mb-4'
-							>
-								<div
-									className={`bg-[#000816] bg-opacity-60 p-6 flex-1 flex flex-col gap-4 rounded-lg border-2 border-primary`}
-								>
-									<div className='flex items-center gap-6'>
-										{/* Imagen solo para participantes */}
-										{currentResult?.results[0].img && (
-											<img
-												src={currentResult?.results[0].img}
-												alt={currentResult?.results[0].name}
-												className='object-cover w-64 h-auto rounded-md'
-											/>
-										)}
-										<div>
-											<h3 className='text-3xl text-primary'>
-												{currentResult?.results[0].clipName ||
-													currentResult?.results[0].name}
-											</h3>
-											{/* Mostrar autor solo para clips */}
-											{currentResult?.results[0].authorName && (
-												<p className='text-white'>
-													Autor: {currentResult?.results[0].authorName}
-												</p>
-											)}
-											<p className='text-white'>
-												Puntos: {currentResult?.results[0].points}
-											</p>
+						{(() => {
+							// Calcular el total de puntos de todos los resultados
+							const totalPoints = currentResult?.results.reduce(
+								(sum, result) => sum + result.points,
+								0
+							);
+
+							return (
+								<>
+									{/* Top 1 */}
+									<div className='flex mb-4'>
+										<div
+											key={
+												currentResult?.results[0].participantId ||
+												currentResult?.results[0].id
+											}
+											className='w-full mb-4'
+										>
+											<div
+												className={`bg-[#000816] bg-opacity-60 p-6 flex-1 flex flex-col gap-4 rounded-lg border-2 border-primary`}
+											>
+												<div className='flex items-center gap-6'>
+													{/* Imagen solo para participantes */}
+													{currentResult?.results[0].img && (
+														<img
+															src={currentResult?.results[0].img}
+															alt={currentResult?.results[0].name}
+															className='object-cover w-64 h-auto rounded-md'
+														/>
+													)}
+													<div>
+														<h3 className='text-3xl text-primary'>
+															{currentResult?.results[0].clipName ||
+																currentResult?.results[0].name}
+														</h3>
+														{/* Mostrar autor solo para clips */}
+														{currentResult?.results[0].authorName && (
+															<p className='text-white'>
+																Autor:{" "}
+																{
+																	currentResult?.results[0]
+																		.authorName
+																}
+															</p>
+														)}
+														<p className='text-white'>
+															Puntos:{" "}
+															{currentResult?.results[0].points}{" "}
+															{totalPoints > 0 && (
+																<span>
+																	(
+																	{(
+																		(currentResult?.results[0]
+																			.points /
+																			totalPoints) *
+																		100
+																	).toFixed(1)}
+																	%)
+																</span>
+															)}
+														</p>
+													</div>
+												</div>
+											</div>
 										</div>
 									</div>
-								</div>
-							</div>
-						</div>
 
-						{/* Resto de los participantes o clips */}
-						<div>
-							{currentResult?.results.map((result, index) => (
-								<div key={result.participantId || result.id}>
-									{index > 0 && (
-										<div className='bg-[#000816] bg-opacity-60 px-4 py-2 rounded-lg mb-2'>
-											<h3 className='text-xl text-primary'>
-												{result.clipName || result.name}
-											</h3>
-											{/* Mostrar autor solo para clips */}
-											{result.authorName && (
-												<p className='m-0 text-white'>
-													Autor: {result.authorName}
-												</p>
-											)}
-											<p className='m-0 text-white'>
-												Puntos: {result.points}
-											</p>
-										</div>
-									)}
-								</div>
-							))}
-						</div>
+									{/* Resto de los participantes o clips */}
+									<div>
+										{currentResult?.results.map((result, index) => (
+											<div key={result.participantId || result.id}>
+												{index > 0 && (
+													<div className='bg-[#000816] bg-opacity-60 px-4 py-2 rounded-lg mb-2'>
+														<h3 className='text-xl text-primary'>
+															{result.clipName || result.name}
+														</h3>
+														{/* Mostrar autor solo para clips */}
+														{result.authorName && (
+															<p className='m-0 text-white'>
+																Autor: {result.authorName}
+															</p>
+														)}
+														<p className='m-0 text-white'>
+															Puntos: {result.points}{" "}
+															{totalPoints > 0 && (
+																<span>
+																	(
+																	{(
+																		(result.points /
+																			totalPoints) *
+																		100
+																	).toFixed(1)}
+																	%)
+																</span>
+															)}
+														</p>
+													</div>
+												)}
+											</div>
+										))}
+									</div>
+								</>
+							);
+						})()}
 					</div>
 				)}
 			</div>
